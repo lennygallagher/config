4649fd0424d3958be17a9d9debcabd239764d367
Robert Brem <brem_robert@hotmail.com>
2017-06-10 09:25:59 +0200

    initial commit

---
+++ startKafka.sh
@@ -0,0 +1,61 @@
+docker stop zk-1 && docker rm zk-1 \
+  && docker stop zk-2 && docker rm zk-2 \
+  && docker stop zk-3 && docker rm zk-3
+
+docker run -d \
+   --net=host \
+   --name=zk-1 \
+   -e ZOOKEEPER_SERVER_ID=1 \
+   -e ZOOKEEPER_CLIENT_PORT=22181 \
+   -e ZOOKEEPER_TICK_TIME=2000 \
+   -e ZOOKEEPER_INIT_LIMIT=5 \
+   -e ZOOKEEPER_SYNC_LIMIT=2 \
+   -e ZOOKEEPER_SERVERS="localhost:22888:23888;localhost:32888:33888;localhost:42888:43888" \
+   confluentinc/cp-zookeeper:3.2.1
+
+docker run -d \
+   --net=host \
+   --name=zk-2 \
+   -e ZOOKEEPER_SERVER_ID=2 \
+   -e ZOOKEEPER_CLIENT_PORT=32181 \
+   -e ZOOKEEPER_TICK_TIME=2000 \
+   -e ZOOKEEPER_INIT_LIMIT=5 \
+   -e ZOOKEEPER_SYNC_LIMIT=2 \
+   -e ZOOKEEPER_SERVERS="localhost:22888:23888;localhost:32888:33888;localhost:42888:43888" \
+   confluentinc/cp-zookeeper:3.2.1
+
+docker run -d \
+   --net=host \
+   --name=zk-3 \
+   -e ZOOKEEPER_SERVER_ID=3 \
+   -e ZOOKEEPER_CLIENT_PORT=42181 \
+   -e ZOOKEEPER_TICK_TIME=2000 \
+   -e ZOOKEEPER_INIT_LIMIT=5 \
+   -e ZOOKEEPER_SYNC_LIMIT=2 \
+   -e ZOOKEEPER_SERVERS="localhost:22888:23888;localhost:32888:33888;localhost:42888:43888" \
+   confluentinc/cp-zookeeper:3.2.1
+
+docker stop kafka-1 && docker rm kafka-1 \
+  && docker stop kafka-2 && docker rm kafka-2 \
+  && docker stop kafka-3 && docker rm kafka-3
+
+docker run -d \
+    --net=host \
+    --name=kafka-1 \
+    -e KAFKA_ZOOKEEPER_CONNECT=localhost:22181,localhost:32181,localhost:42181 \
+    -e KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://localhost:29092 \
+    confluentinc/cp-kafka:3.2.1
+
+docker run -d \
+    --net=host \
+    --name=kafka-2 \
+    -e KAFKA_ZOOKEEPER_CONNECT=localhost:22181,localhost:32181,localhost:42181 \
+    -e KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://localhost:39092 \
+    confluentinc/cp-kafka:3.2.1
+
+ docker run -d \
+     --net=host \
+     --name=kafka-3 \
+     -e KAFKA_ZOOKEEPER_CONNECT=localhost:22181,localhost:32181,localhost:42181 \
+     -e KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://localhost:49092 \
+     confluentinc/cp-kafka:3.2.1

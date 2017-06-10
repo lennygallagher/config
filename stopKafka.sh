2e4cbe688b2ba5056da2558b86b6e76d97f3f9ff
Robert Brem <brem_robert@hotmail.com>
2017-06-10 14:58:56 +0200

    stop kafka as well

--- stopKafka.sh
+++ stopKafka.sh
@@ -3,3 +3,7 @@
 docker stop zk-1 && docker rm zk-1 \
   && docker stop zk-2 && docker rm zk-2 \
   && docker stop zk-3 && docker rm zk-3
+
+docker stop kafka-1 && docker rm kafka-1 \
+  && docker stop kafka-2 && docker rm kafka-2 \
+  && docker stop kafka-3 && docker rm kafka-3

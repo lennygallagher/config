2ed6fd8bf9f7be841785ce3615c3eb0fc1077e3e
tom <tomasz.bieruta@adesso.ch>
2017-06-23 14:32:54 +0200

    registry, rest-proxy und some other tools

--- stopKafka.sh
+++ stopKafka.sh
@@ -8,4 +8,10 @@ docker stop kafka-1 && docker rm kafka-1 \
   && docker stop kafka-2 && docker rm kafka-2 \
   && docker stop kafka-3 && docker rm kafka-3
 
+docker stop kafka-rest && docker rm kafka-rest
+
+docker stop kafka-manager && docker rm kafka-manager
+
+docker stop kafka-topic-ui && docker rm kafka-topic-ui
+
 docker stop schema-reg-1 && docker rm schema-reg-1
